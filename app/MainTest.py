d74dbc98c496e93b95bab7621a796db1d2fd1349
DiegoElRey <dandressantiago@unicesar.edu.co>
2022-10-04 10:49:23 -0500

    test prime

--- app/MainTest.py
+++ app/MainTest.py
@@ -4,17 +4,17 @@ import Main as api
 class MainTest (test.TestCase):
     def setUp(self):
         self.main = api
-    def testShouldReturnTrueIsPrimeNumber(self):
+    def test_ShouldReturnTrueIsPrimeNumber(self):
         self.main = api
-        assert self.main.validate_number(5), True
-    def testShouldReturnFalseIsNotAPrimeNumber(self):
+        self.assertEqual(self.main.validate_number(5), True)
+    def test_ShouldReturnFalseIsNotAPrimeNumber(self):
         self.main = api
-        assert self.main.validate_number(1), False
-    def testShouldReturnFalseIsNotsPrimeNumber(self):
+        self.assertEqual(self.main.validate_number(1), False)
+    def test_ShouldReturnFalseIsNotsPrimeNumber(self):
         self.main = api
-        assert self.main.validate_number(0), False
+        self.assertEqual(self.main.validate_number(0), False)
     def testShouldReturnFalseIsNotIsAPrimeNumber(self):
         self.main = api
-        assert self.main.validate_number(4), False
+        self.assertEqual(self.main.validate_number(4), False)
     
 test.main()
